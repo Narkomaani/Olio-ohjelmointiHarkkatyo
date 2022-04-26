66cb22ca0d198901d823c676af307dc46d2356a6
Narkomaani <77407960+Narkomaani@users.noreply.github.com>
2022-04-27 01:01:50 +0300

    added LoginActivity

--- app/src/main/java/com/example/ht/LoginActivity.java
+++ app/src/main/java/com/example/ht/LoginActivity.java
@@ -2,13 +2,39 @@ package com.example.ht;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.login);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+
+
+    protected void onSuccessfulLogin() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
