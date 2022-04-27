3a0dad53cfa000249f711d9c17e3abb7959781e4
Narkomaani <77407960+Narkomaani@users.noreply.github.com>
2022-04-27 19:48:04 +0300

    added CalendarFragment

---
+++ app/src/main/java/com/example/ht/CalendarFragment.java
@@ -0,0 +1,35 @@
+package com.example.ht;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link CalendarFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class CalendarFragment extends Fragment {
+
+    public CalendarFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static CalendarFragment newInstance() {
+        CalendarFragment fragment = new CalendarFragment();
+        return fragment;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_calendar, container, false);
+    }
+}
