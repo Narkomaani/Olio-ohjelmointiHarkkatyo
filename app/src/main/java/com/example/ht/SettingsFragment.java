11536a709a2d761d82ed213a546ec123b89da7e9
ellaaskeli <ella.askeli@hotmail.com>
2022-05-03 16:13:00 +0300

    Changes to settings

--- app/src/main/java/com/example/ht/SettingsFragment.java
+++ app/src/main/java/com/example/ht/SettingsFragment.java
@@ -1,10 +1,21 @@
 package com.example.ht;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.os.UserManager;
+
+import androidx.annotation.NonNull;
 import androidx.preference.EditTextPreference;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreference;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 
 public class SettingsFragment extends PreferenceFragmentCompat {
@@ -13,16 +24,57 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     private SharedPreferences.Editor editor;
     UserManager userManager;
 
+    public static final String LANG = "lang";
+    private SharedPreferences.OnSharedPreferenceChangeListener preferenceChangeListener;
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
 
-        EditTextPreference emailAddressPreference = findPreference("emailAddress");
-        emailAddressPreference.setSummary("testi");
+        loadLocale();
 
-        EditTextPreference passwordPreference = findPreference("password");
-
+        preferenceChangeListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+            @Override
+            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+                if (s.equals(LANG)) {
+                    Preference langPref = findPreference(s);
+                    langPref.setSummary(sharedPreferences.getString(s, ""));
+                    setLocale("fi");
+                }
+            }
+        };
 
     }
 
+    private void setLocale(String s) {
+        Locale locale = new Locale(s);
+        Locale.setDefault(locale);
+        Configuration configuration = new Configuration();
+        configuration.locale = locale;
+        getActivity().getBaseContext().getResources().updateConfiguration(configuration, getActivity().getBaseContext().getResources().getDisplayMetrics());
+        // save data to shared preferences
+        SharedPreferences.Editor editor = getActivity().getSharedPreferences("Settings", Context.MODE_PRIVATE).edit();
+        editor.putString("my_lang", LANG);
+        editor.apply();
+    }
+
+    // load language saved in shared preferences
+    public void loadLocale() {
+        SharedPreferences prefs = getActivity().getSharedPreferences("Settings", Activity.MODE_PRIVATE);
+        String language = prefs.getString("my_lang", "");
+        setLocale(language);
+    }
+
+    /*@Override
+    public void onResume() {
+        super.onResume();
+
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(preferenceChangeListener);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(preferenceChangeListener);
+    }*/
 }
